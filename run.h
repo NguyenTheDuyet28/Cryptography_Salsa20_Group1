1e84705284d2e2a620947c779a2c732592c46044
NguyenTheDuyet <134566144+NguyenTheDuyet28@users.noreply.github.com>
2025-11-02 21:51:37 +0700

    run header

--- run.h
+++ run.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "salsa20.h"
 
-#define MAX_SIZE 64 // Plaintext tối đa 64 bytes
+#define MAX_PLAINTEXT_BUFFER 1025 
 
+void salsa20_crypt(uint8_t key[32], int key_len, uint8_t nonce[8],
+    uint64_t initial_counter, uint8_t* buffer, size_t length);
 void run_user(void); //Chạy chương trình nhập thủ công
 void run_test_vectors(void); //Chạy chương trình với test vectors
