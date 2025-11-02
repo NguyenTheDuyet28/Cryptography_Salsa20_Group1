f41ab14d2e898b8b2ac1a6b53e67d3a5aff904b3
NguyenTheDuyet <134566144+NguyenTheDuyet28@users.noreply.github.com>
2025-11-02 17:18:25 +0700

    salsa20 header

--- salsa20.h
+++ salsa20.h
@@ -1,11 +1,8 @@
-﻿#pragma once
-#include<stdint.h>
-#include<stddef.h>
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h> 
-#include<ctype.h>
-#include<stdbool.h>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma once
+#include<stdint.h>  
+#include<stddef.h>  
+#include<stdbool.h> 
 
 #define ROUNDS 20
 
@@ -15,12 +12,9 @@
 	c ^= ROTL(b + a, 9), \
 	d ^= ROTL(c + b,13), \
 	a ^= ROTL(d + c,18))
-
 uint32_t U8TO32_LITTLE(const uint8_t* p);  // Chuyển 4 bytes liên tiếp thành 1 word (Litte endian)
 void U32TO8_LITTLE(uint8_t* p, uint32_t v); // Chuyển 1 word thành 4 bytes (Little endian)
 void salsa20_block(uint32_t out[16], uint32_t const in[16]); // Hàm salsa20 block
 void clean_stdin(void); //Loại bỏ ký tự \n
 int hex_char_to_int(char c); // Chuyển ký tự hex thành int
-int hex_string_to_bytes(uint8_t *dst, const char *src, size_t dst_len); // Chuyển chuỗi hex thành mảng byte
-
-
+int hex_string_to_bytes(uint8_t* dst, const char* src, size_t dst_len); // Chuyển chuỗi hex thành mảng byte
